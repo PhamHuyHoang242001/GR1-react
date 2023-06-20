eecc06ff960fd8eca328ac3d299efad7a7969b54
huyhoang242001 <huyhoang.pham@secomus.com>
2023-06-20 21:32:45 +0700

    update article

--- src/component/PaginationArticle.js
+++ src/component/PaginationArticle.js
@@ -1,18 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Pagination, Select } from "antd";
-import * as lung_article from "../data/lung_article_.json";
+// import * as lung_article from "../data/lung_article_.json";
 //import * as colorectal_article from "../data/colorectal_article_.json";
 import LungArticle from "./LungArticle";
 import SeachArticle from "../search/SeachArticle";
+import axios from "axios";
 const { Option } = Select;
 
-let articleData = lung_article;
-const articleConverted = Object.values(articleData);
+// let articleData = lung_article;
+// const articleConverted = Object.values(articleData);
 function PaginationArticle() {
+
   const [currentPage, setCurrentPage] = useState(1);
   const [filterValue, setFilterValue] = useState("All");
-  const [searchData, setSearchData] = useState(articleConverted);
+  const [searchData, setSearchData] = useState(null);
+  const [datas, setDatas] = useState(null);
   const [isSearch, setIsSearch] = useState(false);
+
+  async function fetchData() {
+    try {
+      const response = await axios.get(
+        'https://ut-project-be.vercel.app/api/components'
+      );
+      console.log(response);
+      setDatas(response.data.data);
+      setSearchData(response.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   const pageSize = 5;
 
@@ -32,7 +51,7 @@ function PaginationArticle() {
     setCurrentPage(1);
   };
 
-  const filteredArticles = articleConverted.filter((article) => {
+  const filteredArticles = datas.filter((article) => {
     if (filterValue === "All") {
       return true;
     }
@@ -59,7 +78,7 @@ function PaginationArticle() {
       >
         <div style={{ flex: 1 }}>
           <SeachArticle
-            Articles={articleConverted}
+            Articles={datas}
             setSearchData={handleSearchDataChange}
             setIsSearch={setIsSearch}
             paginate={handlePageChange}
